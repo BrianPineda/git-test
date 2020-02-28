c54b8b5c54c9e7cdcc7e0b6d9981441e9a244268
BrianPineda <BrianPi@magenic.com>
2020-02-28 12:28:27 +0800

    Adding Build folder via drag and drop

---
+++ build/precache-manifest.5c60dbbcfa57679d78fac249a201e6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f92d65ce55c431875e396b1c1d4632",
+    "url": "/git-test/index.html"
+  },
+  {
+    "revision": "14bdddd53e0b42569a64",
+    "url": "/git-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9133338d8d85460a3fca",
+    "url": "/git-test/static/js/2.ba7fbf64.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/git-test/static/js/2.ba7fbf64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14bdddd53e0b42569a64",
+    "url": "/git-test/static/js/main.eb339269.chunk.js"
+  },
+  {
+    "revision": "1fd3eae1b4cd44fa15cd",
+    "url": "/git-test/static/js/runtime-main.daa10f91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
