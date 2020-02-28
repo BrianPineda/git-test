0a830f0150d93afdae292687ff6f2c0cbf5b112c
Brian Pineda <BrianPi@magenic.com>
2020-02-28 13:09:25 +0800

    Test changes

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          {process.env.NODE_ENV}
+          Learn React {process.env.NODE_ENV}
         </a>
       </header>
     </div>
